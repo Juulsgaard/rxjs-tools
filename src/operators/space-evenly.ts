7f6c2555677e2064be2f07016e9581ed27d599b0
Frederik Juulsgaard Sørensen <frederikjs1@hotmail.com>
2022-08-17 18:51:19 +0200

    Add repeating operator

--- src/operators/space-evenly.ts
+++ src/operators/space-evenly.ts
@@ -1,12 +1,26 @@
-import {asyncScheduler, MonoTypeOperatorFunction, Observable, SchedulerLike, Subscription} from "rxjs";
+import {asyncScheduler, MonoTypeOperatorFunction, Observable, SchedulerLike, Subscriber, Subscription, Unsubscribable} from "rxjs";
 
 class Completed {
 }
+
+const completed = new Completed();
+
+class Empty {
+}
+
+const empty = new Empty();
 
 class Failed {
   constructor(public error: any) {
   }
 }
+
+class Value<T> {
+  constructor(public value: T) {
+  }
+}
+
+type Union<T> = Completed | Empty | Failed | Value<T>;
 
 /**
  * Will evenly space out values using a buffer
@@ -17,74 +31,177 @@ export function spaceEvenly<T>(delay: number, scheduler: SchedulerLike = asyncSc
   return (source) => {
     return new Observable(subscriber => {
 
-      let completed = false;
-      let timer: Subscription | undefined;
-      let lastEmit: number|undefined;
-      let buffer: (T | Completed | Failed)[] = [];
-
-      function emit() {
-        timer?.unsubscribe();
-
-        if (buffer.length < 1) return;
-        const value = buffer.shift();
-
-        if (value instanceof Failed) {
-          subscriber.error(value.error);
-          tryEmit();
-          return;
-        }
-
-        if (value instanceof Completed) {
-          subscriber.complete();
-          return;
-        }
-
-        lastEmit = scheduler.now();
-        subscriber.next(value);
-
-        timer = undefined;
-        tryEmit();
-      }
-
-      function tryEmit() {
-        if (timer) return;
-        if (buffer.length < 1) return;
-        if (!lastEmit) {
-          emit();
-          return;
-        }
-        const now = scheduler.now();
-        const timeSinceEmit = now - lastEmit;
-        if (timeSinceEmit >= delay) {
-          emit();
-          return;
-        }
-
-        timer = scheduler.schedule(() => emit(), delay - timeSinceEmit);
-      }
+      const queue: Union<T>[] = [];
+      const emitter = new TimedEmitter(subscriber, delay, () => queue.length > 0, () => {
+        if (queue.length > 0) return new Value(queue.shift());
+        return empty;
+      }, scheduler);
 
       const sub = source.subscribe({
         next(value) {
-          if (completed) return;
-          buffer.push(value);
-          tryEmit();
+          queue.push(value);
+          emitter.emit();
         },
         error: error => {
-          if (completed) return;
-          buffer.push(new Failed(error));
-          tryEmit();
+          queue.push(new Failed(error));
+          emitter.emit();
         },
         complete: () => {
-          completed = true;
-          buffer.push(Completed);
-          tryEmit();
+          queue.push(completed);
+          emitter.emit();
         }
       });
 
       return () => {
         sub.unsubscribe();
-        timer?.unsubscribe();
+        emitter?.unsubscribe();
       }
     });
   };
+}
+
+/**
+ * Will evenly space out values using a buffer.
+ * If no values are available in the buffer, a value will be pulled from the value history.
+ * History will only be used if more than `minHistory` values have been processed
+ * @param delay - The min delay to maintain between values in ms
+ * @param maxHistory - The maximum amount of entries to keep in memory
+ * @param minHistory - The minimum amount of entries before history will be used
+ * @param scheduler - The scheduler
+ */
+export function spaceEvenlyRepeat<T>(delay: number, maxHistory = 100, minHistory = 1, scheduler: SchedulerLike = asyncScheduler): MonoTypeOperatorFunction<T> {
+  maxHistory = Math.max(1, maxHistory);
+  minHistory = Math.min(maxHistory, Math.max(1, minHistory));
+
+  return (source) => {
+    return new Observable(subscriber => {
+
+      const queue: (T | Completed | Failed)[] = [];
+
+      let history: T[] = [];
+      let futureHistory: T[] = [];
+
+      function getValue(): Union<T> {
+        // If value is queued, return it
+        if (queue.length > 0) return new Value(queue.shift());
+
+        // If history is empty try loading future history
+        if (history.length < 1) {
+          if (futureHistory.length < minHistory) return empty;
+          history = futureHistory;
+          futureHistory = [];
+        }
+
+        // Return next history value
+        return new Value(history.shift());
+      }
+
+      const emitter = new TimedEmitter(
+        subscriber,
+        delay,
+        () => {
+          if (queue.length > 0) return true;
+          if (history.length > 0) return true;
+          return futureHistory.length > minHistory;
+        },
+        () => {
+          const value = getValue();
+
+          // Save generated values to future history
+          if (value instanceof Value) {
+            futureHistory.push(value.value);
+            if (futureHistory.length > maxHistory) futureHistory.shift();
+          }
+
+          return value;
+        },
+        scheduler
+      );
+
+      const sub = source.subscribe({
+        next(value) {
+          queue.push(value);
+          emitter.emit();
+        },
+        error: error => {
+          queue.push(new Failed(error));
+          emitter.emit();
+        },
+        complete: () => {
+          queue.push(completed);
+          emitter.emit();
+        }
+      });
+
+      return () => {
+        sub.unsubscribe();
+        emitter?.unsubscribe();
+      }
+    });
+  };
+}
+
+class TimedEmitter<T> implements Unsubscribable {
+
+  private timer: Subscription | undefined;
+  private lastEmit: number | undefined;
+
+  constructor(
+    private subscriber: Subscriber<T>,
+    private delay: number,
+    private hasValue: () => boolean,
+    private getValue: () => Union<T>,
+    private scheduler: SchedulerLike = asyncScheduler
+  ) {
+  }
+
+  private emitInternal() {
+    this.timer?.unsubscribe();
+
+    const value = this.getValue();
+
+    if (value instanceof Empty) return;
+
+    if (value instanceof Failed) {
+      this.subscriber.error(value.error);
+      this.emit();
+      return;
+    }
+
+    if (value instanceof Completed) {
+      this.subscriber.complete();
+      return;
+    }
+
+    this.lastEmit = this.scheduler.now();
+    this.subscriber.next(value.value);
+
+    this.timer = undefined;
+    this.emit();
+  }
+
+  emit() {
+    if (this.timer) return;
+
+    if (!this.hasValue()) return;
+
+    if (!this.lastEmit) {
+      this.emitInternal();
+      return;
+    }
+
+    const now = this.scheduler.now();
+    const timeSinceEmit = now - this.lastEmit;
+
+    if (timeSinceEmit >= this.delay) {
+      this.emitInternal();
+      return;
+    }
+
+    this.timer = this.scheduler.schedule(() => this.emitInternal(), this.delay - timeSinceEmit);
+  }
+
+  unsubscribe(): void {
+    this.timer?.unsubscribe();
+  }
 }
