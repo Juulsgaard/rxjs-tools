bd99e203ff52db0ea9a8e83e3c5eaec7582838f0
Frederik Sørensen <frederikjs1@hotmail.com>
2022-11-07 12:07:37 +0100

    Add loadingFirst operator

--- src/operators/state.ts
+++ src/operators/state.ts
@@ -1,4 +1,4 @@
-import {MonoTypeOperatorFunction, Subject, tap} from "rxjs";
+import {last, MonoTypeOperatorFunction, Subject, tap} from "rxjs";
 import {isFunction} from "@consensus-labs/ts-tools";
 
 /**
@@ -16,5 +16,35 @@ export function subscribed<T>(state: Subject<boolean>|((subscribed: boolean) => 
   return tap<T>({
    subscribe: () => setState(true),
     finalize: () => setState(false)
+  });
+}
+
+/**
+ * Projects the state of the observable
+ * State is true while the observable is active, but hasn't emitted a value
+ * @param onState - Callback triggered whenever the state changes
+ */
+export function loadingFirst<T>(onState: (loadingFirst: boolean) => void): MonoTypeOperatorFunction<T>
+/**
+ * Projects the state of the observable
+ * State is true while the observable is active, but hasn't emitted a value
+ * @param state$ - A subject that will receive the current state
+ */
+export function loadingFirst<T>(state$: Subject<boolean>): MonoTypeOperatorFunction<T>
+export function loadingFirst<T>(state: Subject<boolean>|((loadingFirst: boolean) => void)): MonoTypeOperatorFunction<T> {
+
+  const baseSetState = isFunction(state) ? state : state.next.bind(state);
+
+  let lastState = false;
+  const setState = (state: boolean) => {
+    if (state === lastState) return;
+    lastState = state;
+    baseSetState(state);
+  };
+
+  return tap<T>({
+    subscribe: () => setState(true),
+    next: () => setState(false),
+    finalize: () => setState(false),
   });
 }
