b5154c9089673d0aa5bbdfcf5a5d04b0a00785e3
Frederik Sørensen <frederikjs1@hotmail.com>
2022-11-13 23:28:46 +0100

    Extend permanent cache so it can reset on value emission

--- src/operators/cache.ts
+++ src/operators/cache.ts
@@ -1,4 +1,4 @@
-import {Observable, OperatorFunction, ReplaySubject, share, switchMap, timer, Unsubscribable} from "rxjs";
+import {MonoTypeOperatorFunction, Observable, OperatorFunction, ReplaySubject, share, switchMap, timer, Unsubscribable} from "rxjs";
 
 /**
  * Cache the last value of the observable
@@ -31,14 +31,45 @@ export function persistentCache<T>(duration = 1000) {
  * Cache the last value of the observable
  * Will cache permanently. Will stay alive even with 0 subscribers
  * Complete observable to dispose
+ * @param releaseOnValue - If true the cache will reset when there are no subscribers and a new value is emitted from the source
  */
-export function permanentCache<T>() {
-  return share<T>({
-    connector: () => new ReplaySubject(1, Infinity),
-    resetOnError: false,
-    resetOnComplete: false,
-    resetOnRefCountZero: false
-  });
+export function permanentCache<T>(releaseOnValue = false): MonoTypeOperatorFunction<T> {
+  return source => {
+
+    let subject: ReplaySubject<T>|undefined;
+    let sub: Unsubscribable|undefined;
+    let refCount = 0;
+
+    const getSubject = () => {
+      if (subject) return subject;
+
+      subject = new ReplaySubject<T>(1, Infinity);
+
+      sub = source.subscribe({
+        next: value => {
+          if (!subject) return;
+          if (releaseOnValue && refCount < 1) {
+            subject = undefined;
+            sub?.unsubscribe();
+            sub = undefined;
+            return;
+          }
+          subject.next(value);
+        },
+        error: err => subject?.error(err),
+        complete: () => subject?.complete()
+      });
+
+      return subject;
+    };
+
+    return new Observable(subscriber => {
+      refCount++;
+      subscriber.add(() => refCount--);
+      const subject = getSubject();
+      return subject.subscribe(subscriber);
+    });
+  }
 }
 
 /**
@@ -102,8 +133,9 @@ export function voidablePersistentCache<T, TOut>(mapFunc: (value: T) => Observab
  * Complete observable to dispose
  * Every new value voids the observable until a value is emitted from the mapped observable
  * @param mapFunc - Function to generate an observable which populates the main observable after being voided
+ * @param releaseOnValue - If true the cache will reset when there are no subscribers and a new value is emitted from the source
  */
-export function voidablePermanentCache<T, TOut>(mapFunc: (value: T) => Observable<TOut>): OperatorFunction<T, TOut> {
+export function voidablePermanentCache<T, TOut>(mapFunc: (value: T) => Observable<TOut>, releaseOnValue = false): OperatorFunction<T, TOut> {
   return source => source.pipe(
     cachedSwitchMap(x => mapFunc(x)),
     permanentCache(),
