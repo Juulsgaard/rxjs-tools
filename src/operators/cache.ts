e2c3d99628a2cf61a924cb9472f98939d3cbba2a
Frederik Sørensen <frederikjs1@hotmail.com>
2022-11-13 20:55:44 +0100

    Add Voidable caches

--- src/operators/cache.ts
+++ src/operators/cache.ts
@@ -1,4 +1,5 @@
-import {ReplaySubject, share, timer} from "rxjs";
+import {Observable, of, OperatorFunction, ReplaySubject, share, switchMap, timer} from "rxjs";
+import {map} from "rxjs/operators";
 
 /**
  * Cache the last value of the observable
@@ -39,4 +40,45 @@ export function permanentCache<T>() {
     resetOnComplete: false,
     resetOnRefCountZero: false
   });
+}
+
+/**
+ * Cache the last value of the observable
+ * Will reset if all subscribers unsubscribe
+ * Every new value voids the observable until a value is emitted from the mapped observable
+ * @param mapFunc - Function to generate an observable which populates the main observable after being voided
+ */
+export function voidableCache<T, TOut>(mapFunc: ($: Observable<T>) => Observable<TOut>): OperatorFunction<T, TOut> {
+  return source => source.pipe(
+    map(x => mapFunc(of(x)).pipe(permanentCache())),
+    cache(),
+    switchMap(x => x));
+}
+
+/**
+ * Cache the last value of the observable
+ * Will stay alive without subscribers for the duration specified
+ * Every new value voids the observable until a value is emitted from the mapped observable
+ * @param mapFunc - Function to generate an observable which populates the main observable after being voided
+ * @param duration - Keep alive duration is ms
+ */
+export function voidablePersistentCache<T, TOut>(mapFunc: ($: Observable<T>) => Observable<TOut>, duration = 1000): OperatorFunction<T, TOut> {
+  return source => source.pipe(
+    map(x => mapFunc(of(x)).pipe(permanentCache())),
+    persistentCache(duration),
+    switchMap(x => x));
+}
+
+/**
+ * Cache the last value of the observable
+ * Will cache permanently. Will stay alive even with 0 subscribers
+ * Complete observable to dispose
+ * Every new value voids the observable until a value is emitted from the mapped observable
+ * @param mapFunc - Function to generate an observable which populates the main observable after being voided
+ */
+export function voidablePermanentCache<T, TOut>(mapFunc: ($: Observable<T>) => Observable<TOut>): OperatorFunction<T, TOut> {
+  return source => source.pipe(
+    map(x => mapFunc(of(x)).pipe(permanentCache())),
+    permanentCache(),
+    switchMap(x => x));
 }
