fee9dc2c408f53764299d9ae733a490f19146bba
Frederik Sørensen <frederikjs1@hotmail.com>
2022-11-14 14:19:58 +0100

    Add Unique Cache

--- src/operators/cache.ts
+++ src/operators/cache.ts
@@ -73,6 +73,60 @@ export function permanentCache<T>(releaseOnValue = false): MonoTypeOperatorFunct
 }
 
 /**
+ * Experimental cache operator that will release once it has no subscribers.
+ * It will still store the latest value, and will emit that old value to new subscribers.
+ * If the first source value after reset is the same as the latest, then it will not be re-emitted.
+ * The purpose is to have a cache that unsubscribes, but still doesn't re-evaluate unnecessarily
+ */
+export function uniqueCache<T>(): MonoTypeOperatorFunction<T> {
+  return source => {
+
+    let subject = new ReplaySubject<T>(1, Infinity);
+    let sub: Unsubscribable|undefined;
+    let refCount = 0;
+    let hasValue = false;
+    let lastValue: T|undefined;
+
+    return new Observable(subscriber => {
+      refCount++;
+
+      if (!sub) {
+        let first = true;
+
+        sub = source.subscribe({
+          next: value => {
+            if (first) {
+              first = false;
+              if (hasValue) {
+                if (lastValue === value) return;
+              }
+            }
+
+            hasValue = true;
+            lastValue = value;
+            subject.next(value);
+          },
+          error: err => subject.error(err),
+          complete: () => subject.complete()
+        });
+      }
+
+      const subscription = subject.subscribe(subscriber);
+
+      return () => {
+        subscription.unsubscribe();
+        refCount--;
+
+        if (refCount === 0) {
+          sub?.unsubscribe();
+          sub = undefined;
+        }
+      };
+    });
+  }
+}
+
+/**
  * A method that will map a value to a subscribable type
  * The subscribable will be automatically cached using a ReplaySubject
  * The subscribable is unsubscribed from on unsubscription or when a new value is mapped
