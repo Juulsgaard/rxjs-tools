b06ce61fe20be41030dc502e2554d578d356b90d
Frederik Sørensen <frederikjs1@hotmail.com>
2023-05-19 14:08:24 +0200

    Add new filter operator

--- src/operators/filters.ts
+++ src/operators/filters.ts
@@ -1,6 +1,17 @@
-import {OperatorFunction} from "rxjs";
+import {MonoTypeOperatorFunction, Observable, OperatorFunction} from "rxjs";
 import {filter} from "rxjs/operators";
 
 export function notNull<T>(): OperatorFunction<T, NonNullable<T>> {
   return source => source.pipe(filter((value): value is NonNullable<T> => value != undefined));
+}
+
+/**
+ * Filter out synchronously emitted values
+ */
+export function notSync<T>(): MonoTypeOperatorFunction<T> {
+  return source => new Observable(subscriber => {
+    let init = true;
+    const sub = source.pipe(filter(() => !init)).subscribe(subscriber);
+    init = false;
+  });
 }
